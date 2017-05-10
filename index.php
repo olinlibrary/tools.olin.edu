85fb14e0dc79940ad80f952fe7451a47b9bbb245
amhoov <amhoov@gmail.com>
2017-05-09 21:39:58 -0400

    Updated index.php with configuration parameters.

--- index.php
+++ index.php
@@ -12,14 +12,18 @@ $f3->set('VERSION','1.0');
 $f3->set('CACHE', true); // Required For Sessions
 new Session();
 
+$params=parse_ini_file('./tools_conf.ini');
+
 // Initialize Database & RedBeanPHP ORM
 require('vendor/gabordemooij/redbean/rb-p533.php');
-//R::setup('mysql:host=localhost;dbname=tools','tools','TOOLS_PASSWORD');
-R::setup('mysql:host=localhost;dbname=tools','tools','p8iVCPzkqA');
+R::setup('mysql:host=localhost;dbname='.$params['db_name'],$params['db_user'],$params['db_pwd']);
 R::freeze(true);
 define('REDBEAN_MODEL_PREFIX', '\\Models\\');
+$schema = R::getDuplicationManager()->getSchema();
+R::getDuplicationManager()->setTables($schema);
 
 require 'app/helper.php';
 include "app/routes.php";
+ini_set('memory_limit', '512M');
 
 $f3->run();
